bad27c9fd924c5134ae20f7838e615a42689479a
Laruma Alli <alliokelanre@gmail.com>
2023-03-11 20:31:41 -0600

    Add project files.

---
+++ itemtracker/Migrations/20230311025139_addmenu.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace itemtracker.Migrations
+{
+    public partial class addmenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Expenses");
+
+            migrationBuilder.RenameColumn(
+                name: "ExpenseName",
+                table: "Expenses",
+                newName: "Menu");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Allergy",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BussinessUnit",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Allergy",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "BussinessUnit",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Expenses");
+
+            migrationBuilder.RenameColumn(
+                name: "Menu",
+                table: "Expenses",
+                newName: "ExpenseName");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Amount",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ itemtracker/Models/Expense.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace itemtracker.Models
+{
+    public class Expense
+    { 
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Full Name")]
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        //[Range(1,int.MaxValue, ErrorMessage = "Amount must be gretaer than  0!")]
+        [DisplayName("Bussiness Unit")]
+        public string BussinessUnit { get; set; }
+        [Required]
+        public string Menu { get; set; }
+        public string Allergy { get; set; }
+        public DateTime Date { get; set; }
+
+
+
+    }
+}
